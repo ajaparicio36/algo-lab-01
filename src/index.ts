15a200912444ffe4c68e060daf5a2d1077c74fc2
AJ Aparicio <anthonyjohn.aparicio-22@cpu.edu.ph>
2024-01-30 22:31:27 +0800

    feat: create main app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log("First commit");
+import { PercolationStats } from "./PercolationStats";
+import { Percolation } from "./Percolation";
+import * as readlineSync from "readline-sync";
+
+// Monte Carlo simulation and statistics for percolation
+let gridSize = Number(readlineSync.question("Enter grid size: "));
+let trials = Number(readlineSync.question("Enter number of trials: "));
+let ps = new PercolationStats(gridSize, trials);
+
+console.log(`\nStatistics for grid size ${gridSize}`);
+console.log(`\nMean: ${ps.getMean()}`);
+console.log(`Standard Deviation: ${ps.getStdDev()}`);
+console.log(`Lower bound of 95% confidence interval: ${ps.confidenceLo()}`);
+console.log(`Upper bound of 95% confidence interval: ${ps.confidenceHi()}`);
+
+// visualize grid percolation
+let probability = Number(readlineSync.question("Enter probability: "));
+let p = new Percolation(gridSize);
+
+for (let i = 0; i < gridSize; i++) {
+  for (let j = 0; j < gridSize; j++) {
+    p.openWithProbability(i, j, probability);
+  }
+}
+
+p.printGrid();
+console.log("Does the grid percolate? ", p.percolates());
